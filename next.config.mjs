0b17954de9575e072d2508cb95c9a8679cf63ce9
Wander Costa <wanderfernando@gmail.com>
2024-04-28 18:08:43 -0300

    fix: nextjs config images domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,9 @@
-import { hostname } from "os";
-
 const hostnames = ["d3u1babc6tucfz.cloudfront.net"];
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: hostnames.map((hostname) => ({
-      protocol: "https",
-      hostname,
-    })),
+    domains: hostnames,
   },
 };
 
